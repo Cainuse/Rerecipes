e08515c91570f36b87b66334accae2e9659cadfb
Jacky <jktse@sfu.ca>
2019-07-08 21:07:21 -0700

    Added HTTP package, Added a simply layout on the first screen to test api, a simple button to call api to test

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,43 @@
-import 'package:flutter/material.dart';
+import "dart:async";
 
-main(){
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+main() {
+  runApp(new MaterialApp(
+    home: new HomePage(),
+  ));
 
 }
 
-class MyApp extends StatelessWidget{
-  build(context){
-    return MaterialApp();
+class HomePage extends StatefulWidget{
+  @override
+  HomePageState createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage>{
+
+  Future<String>getData() async{
+    http.Response response = await http.get(
+      "https://www.food2fork.com/api/search",
+      headers: {
+        "key": "8c3c655ad410ef1edbbfb9622eec71a5",
+        "q": "Chicken Breast, Salt, Pepper",
+        "sort": "r"
+      }
+    );
+
+    print(response.body);
+  }
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text("Test Api"),
+          onPressed: getData,
+        ),
+      ),
+    );
   }
 }
